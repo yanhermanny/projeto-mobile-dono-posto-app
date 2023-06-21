9ff6d0f41c8679732440f6e80bf252a800e1373a
Gabriel Rodrigues Santos <80860021+Rodriguestensor@users.noreply.github.com>
2023-06-20 23:57:59 -0300

    Adiciona DTOs e Enums (#1)
    * Primeiro commit
    
    * Ajusta estrutura de pastas
    
    ---------
    
    Co-authored-by: Matias <gabrielguesdriro@hotmail.com>
    Co-authored-by: Yan Hermanny <88159884+yanhermanny@users.noreply.github.com>

---
+++ Models/DTOs/LiquidoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace projeto_mobile_dono_posto_app.Models.DTOs
+{
+	public class LiquidoDto
+	{
+		public int? Id { get; set; }
+		public string Nome { get; set; }
+		public double ValorUnitario { get; set; }
+	}
+}
---
+++ Models/Enums/Enums.cs
@@ -0,0 +1,25 @@
+﻿namespace projeto_mobile_dono_posto_app.Models.Enums
+{
+	public class Enums
+	{
+
+	}
+	public enum PerfilEnum
+	{
+		// Adicione os valores do enum PerfilEnum aqui
+		Administrador = 1,
+		Cliente = 2,
+		DonoPosto = 3,
+		FuncionarioPosto = 4
+	}
+
+	public enum StatusEnum
+	{
+		// Adicione os valores do enum StatusEnum aqui
+		Pendente = 1,
+		Aprovado = 2,
+		Reprovado = 3,
+		Bloqueado = 4,
+		Cancelado = 5
+	}
+}
