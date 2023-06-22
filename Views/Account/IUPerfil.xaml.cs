123fb010e011f2237763a9f1e19074c56c8c194e
Matias <gabrielguesdriro@hotmail.com>
2023-06-22 19:45:28 -0300

    telas lista de funcionarios e logo do aplicativo

---
+++ Views/Account/IUPerfil.xaml.cs
@@ -0,0 +1,9 @@
+namespace projeto_mobile_dono_posto_app.Views.Account;
+
+public partial class IUPerfil : ContentPage
+{
+	public IUPerfil()
+	{
+		InitializeComponent();
+	}
+}
--- Views/Funcionarios/IUListaFuncionarios.xaml.cs
+++ Views/Funcionarios/IUListaFuncionarios.xaml.cs
@@ -1,9 +1,54 @@
+using System.Windows.Input;
+
 namespace projeto_mobile_dono_posto_app.Views.Funcionarios;
 
 public partial class IUListaFuncionarios : ContentPage
 {
-	public IUListaFuncionarios()
+    public ICommand SearchCommand { get; set; }
+
+    public IUListaFuncionarios()
 	{
 		InitializeComponent();
-	}
+	
+        List<Funcionario> funcionarios = new List<Funcionario>()
+            {
+                new Funcionario { Nome = "João", NomePosto = "POSTO 1", EnderecoPosto = "RUA Tecnologia" },
+                new Funcionario { Nome = "Maria", NomePosto = "POSTO 1", EnderecoPosto = "RUA Design" },
+                new Funcionario { Nome = "Pedro", NomePosto = "POSTO 1", EnderecoPosto = "RUA Gestão" },
+                new Funcionario { Nome = "Pedro", NomePosto = "POSTO 1", EnderecoPosto = "RUA Gestão" },
+                new Funcionario { Nome = "Pedro", NomePosto = "POSTO 1", EnderecoPosto = "RUA Gestão" },
+                new Funcionario { Nome = "Pedro", NomePosto = "POSTO 1", EnderecoPosto = "RUA Gestão" }
+            };
+
+        lvFuncionarios.ItemsSource = funcionarios;
+
+}
+private string selectedItem;
+
+    public string SelectedItem
+    {
+        get => selectedItem;
+        set
+        {
+            selectedItem = value;
+            // Lógica adicional ao selecionar um item
+        }
+    }
+    private void ExecuteSearch()
+    {
+        // Lógica de pesquisa aqui
+    }
+    public class Funcionario
+{
+    public string Nome { get; set; }
+    public string NomePosto { get; set; }
+    public string EnderecoPosto { get; set; }
+}
+    private void OnSearchButtonPressed(object sender, EventArgs e)
+    {
+        var searchBar = (SearchBar)sender;
+        var searchTerm = searchBar.Text;
+
+        // Lógica de pesquisa aqui
+    }
 }
